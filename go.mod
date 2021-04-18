bd8e2ba8a7b3cd92d587c57007f7e509aff2973a
wywangye <wywangye@didiglobal.com>
2021-04-18 23:12:23 +0800

    【Sentinel-Go】：流控规则持久化|单节点

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/StackExchange/wmi v0.0.0-20210224194228-fe8f1750fd46 // indirect
 	github.com/alibaba/sentinel-golang v1.0.2
-	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/alibaba/sentinel-golang/pkg/datasource/nacos v0.0.0-20210222155611-2af866088578
+	github.com/nacos-group/nacos-sdk-go v1.0.7
 )
--- sentinel-go-sample-provider/flow/qps/qps_limit_example.go
+++ sentinel-go-sample-provider/flow/qps/qps_limit_example.go
@@ -6,14 +6,37 @@ import (
 	"github.com/alibaba/sentinel-golang/core/base"
 	"github.com/alibaba/sentinel-golang/core/config"
 	"github.com/alibaba/sentinel-golang/core/flow"
+	"github.com/alibaba/sentinel-golang/ext/datasource"
 	"github.com/alibaba/sentinel-golang/logging"
+	"github.com/alibaba/sentinel-golang/pkg/datasource/nacos"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
 	"log"
 	"net/http"
 	"runtime"
 	"strings"
 )
 
+const flowDataIdPostfix = "-sentinel-flow" // 流控规则配置文件后缀
+const groupId = "DEFAULT_GROUP"            // 配置文件所属分组
+const namespace = ""                       // 配置文件所属命名空间
+
 const testFlowRuleUrl = "/flow"
+
+// Nacos服务配置信息
+var sc = []constant.ServerConfig{
+	{
+		ContextPath: "/nacos",
+		Port:        8848,
+		IpAddr:      "localhost",
+		Scheme:      "http",
+	},
+}
+
+// Nacos客户端配置信息
+var cc = constant.ClientConfig{
+	TimeoutMs: 5000,
+}
 
 func main() {
 	initAll()
@@ -21,8 +44,11 @@ func main() {
 
 func initAll() {
 	sentinelInit()
-	// 创建流控规则：资源名 sayHello 阈值 2 统计间隔 1000毫秒
-	createFlowRule("testFlowRule", 2, 1000)
+	//// 创建流控规则：资源名 sayHello 阈值 2 统计间隔 1000毫秒
+	//createFlowRule("testFlowRule", 2, 1000)
+
+	// 初始化Nacos，而非手动创建规则
+	nacosInit()
 	httpInit()
 }
 
@@ -44,6 +70,39 @@ func sentinelInit() {
 	err := sentinel.InitWithConfig(conf)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+// 初始化Nacos
+func nacosInit() {
+	// 生成nacos config client(配置中心客户端)
+	client, err := clients.CreateConfigClient(map[string]interface{}{
+		"serverConfigs": sc,
+		"clientConfig":  cc,
+	})
+	if err != nil {
+		fmt.Printf("Fail to create client, err: %+v", err)
+		return
+	}
+
+	// 注册流控规则Handler
+	h := datasource.NewFlowRulesHandler(datasource.FlowRuleJsonArrayParser)
+
+	// 创建NacosDataSource数据源
+	// groupId 对应在nacos中创建配置文件的group
+	// dataId 对应在nacos中创建配置文件的dataId，一定要和Nacos配置中心中的配置文件名一致，否则无法匹配
+	dataId := "sentinel-go-sample" + flowDataIdPostfix
+	nds, err := nacos.NewNacosDataSource(client, groupId, dataId, h)
+	if err != nil {
+		fmt.Printf("Fail to create nacos data source client, err: %+v", err)
+		return
+	}
+
+	//nacos数据源初始化
+	err = nds.Initialize()
+	if err != nil {
+		fmt.Printf("Fail to initialize nacos data source client, err: %+v", err)
+		return
 	}
 }
 
